a5acc1676bbf70a88408bf707c216203be0373a2
bogdan-popescu-uipath <bogdan.popescu@uipath.com>
2017-10-16 14:49:18 +0300

    Removed Script Activities and Added Google Spreadsheet Activities

---
+++ Google Spreadsheet/GoogleSpreadsheet.Activities/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleSpreadsheet.Activities
+{
+    internal class Logger
+    {
+        private static Logger _instance;
+        private TraceSource _traceSource;
+
+        private Logger()
+        {
+
+            _traceSource = new TraceSource("Workflow");
+        }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Info(string message)
+        {
+            _traceSource.TraceEvent(TraceEventType.Information, 0, message);
+        }
+    }
+}
---
+++ GoogleSpreadsheet.Activities.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoogleSpreadsheet.Activities.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoogleSpreadsheet.Activities.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd10478c-3521-4088-bad7-256d7d099912")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
