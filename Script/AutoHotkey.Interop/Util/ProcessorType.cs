77014c0aeefbc0bcec5a1ddc7e66b3d705a27658
bogdan-popescu-uipath <bogdan.popescu@uipath.com>
2017-11-13 14:22:57 +0200

    Revert "Removed Script Activities and Added Google Spreadsheet Activities"
    This reverts commit a5acc1676bbf70a88408bf707c216203be0373a2.

---
+++ Script/AutoHotkey.Interop/Util/ProcessorType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AutoHotkey.Interop.Util
+{
+    internal static class ProcessorType
+    {
+        public static bool Is64Bit() {
+            return IntPtr.Size == 8;
+        }
+        public static bool Is32Bit() {
+            return IntPtr.Size == 4;
+        }
+
+    }
+}
