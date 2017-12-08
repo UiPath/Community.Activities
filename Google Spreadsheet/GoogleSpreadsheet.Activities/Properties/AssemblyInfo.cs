dcbf9ac155d2ab167a711b1778ec86bf477e99e6
florinake <florin@uipath.com>
2017-12-08 20:11:29 +0200

    fix versioning and include packaging file

--- Google Spreadsheet/GoogleSpreadsheet.Activities/Properties/AssemblyInfo.cs
+++ Google Spreadsheet/GoogleSpreadsheet.Activities/Properties/AssemblyInfo.cs
@@ -1,18 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GoogleApplicationScopeActivity")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GoogleSpreadsheet.Activities")]
+[assembly: AssemblyDescription("GoogleSpreadsheet activities designers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GoogleApplicationScopeActivity")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities", "GoogleSpreadsheet")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -22,15 +19,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("42bf8242-66ac-43b3-827a-f69de1a15310")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
