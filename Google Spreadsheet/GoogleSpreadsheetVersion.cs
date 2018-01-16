4d1663a6cde78e93e19fda8904f589fa3f429c02
bogdan-popescu-uipath <bogdan.popescu@uipath.com>
2018-01-16 11:18:30 +0100

    Merge Google Spreadsheet Activities  (#9)
    Add support for Google Spreadsheet Activities

---
+++ Google Spreadsheet/GoogleSpreadsheetVersion.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Windows.Markup;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: XmlnsPrefix("http://schemas.uipath.com/workflow/activities", "ui")]
+[assembly: AssemblyVersion("1.0.2")]
