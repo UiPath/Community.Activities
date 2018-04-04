b5aab5c4c7ddae60db7bf51af629b198bc191181
florinake <florin@uipath.com>
2018-04-04 13:35:54 +0300

    publish credentials activities

---
+++ Credentials/UiPath.Credentials.Activities/DeleteCredential.cs
@@ -0,0 +1,20 @@
+﻿using CredentialManagement;
+using System.Activities;
+using System.ComponentModel;
+
+namespace UiPath.Credentials.Activities
+{
+    [Category("System.Credentials")]
+    public class DeleteCredential : CodeActivity<bool>
+    {
+        [RequiredArgument]
+        [Category("Input")]
+        public InArgument<string> Target { get; set; }
+
+        protected override bool Execute(CodeActivityContext context)
+        {
+            Credential credential = new Credential { Target = Target.Get(context) };
+            return credential.Delete();
+        }
+    }
+}
---
+++ Credentials/UiPath.Credentials.Activities/GetSecureCredential.cs
@@ -0,0 +1,43 @@
+﻿using CredentialManagement;
+using System.Activities;
+using System.ComponentModel;
+using System.Security;
+
+namespace UiPath.Credentials.Activities
+{
+    [Category("System.Credentials")]
+    public class GetSecureCredential : CodeActivity<bool>
+    {
+        [RequiredArgument]
+        [Category("Input")]
+        public InArgument<string> Target { get; set; }
+
+        [Category("Input")]
+        public CredentialType CredentialType { get; set; }
+
+        [Category("Input")]
+        public PersistanceType PersistanceType { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> Username { get; set; }
+
+        [Category("Output")]
+        public OutArgument<SecureString> Password { get; set; }
+
+        public GetSecureCredential()
+        {
+            CredentialType = CredentialType.Generic;
+            PersistanceType = PersistanceType.Enterprise;
+        }
+
+        protected override bool Execute(CodeActivityContext context)
+        {
+            Credential credential = new Credential { Target = Target.Get(context), Type = CredentialType, PersistanceType = PersistanceType };
+            var result = credential.Load();
+            if (!result) return false;
+            Username.Set(context, credential.Username);
+            Password.Set(context, credential.SecurePassword);
+            return true;
+        }
+    }
+}
