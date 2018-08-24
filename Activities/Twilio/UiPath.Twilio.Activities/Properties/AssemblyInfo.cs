ab5dcc686c65d94181e74f84cd0f568f51f559ae
sorinconstantin <constantinsorin95@gmail.com>
2018-08-24 17:46:36 +0300

    Add Twilio activities code
    UI-14077

---
+++ Activities/Twilio/UiPath.Twilio.Activities/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UiPath.Twilio.Activities")]
+[assembly: AssemblyDescription("Windows Workflow Activities for Twilio tasks like sending SMS messages or making outbound phone calls")]
+[assembly: AssemblyConfiguration("")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/twilio", "UiPath.Twilio.Activities")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c7b3fe3-8f7c-46c4-abf2-b7b1a0e70b77")]
+
+//[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
