7e667c21364d2e3b647b8c94001b6f6bcdeeeff7
petcua1 <petcua1@yahoo.com>
2020-11-12 19:11:25 +0200

    Removed unused reference

--- Activities/Database/UiPath.Database/DatabaseConnection.cs
+++ Activities/Database/UiPath.Database/DatabaseConnection.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.Odbc;
-using System.Data.OleDb;
 using System.Text;
 using UiPath.Database.Properties;
 
