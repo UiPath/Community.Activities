8b6a5423fe85de889416e5130ade16740e8b0a97
alcxs <alexandru.stefan@24software.ro>
2021-01-27 17:22:08 +0200

    added missing assembly info files for Java

---
+++ Activities/Java/UiPath.Java.Activities.Design/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/java", "UiPath.Java.Activities.Design")]
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("5e102b9e-dcef-4d8b-8a02-3fdf0fcc0d59")]
