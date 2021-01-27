111fcd8ae7a8c1acd6a723c10bf4d93cb27d8d55
alcxs <alexandru.stefan@24software.ro>
2021-01-27 17:35:52 +0200

    added missing FTP assembly info

---
+++ Activities/FTP/UiPath.FTP.Activities.Design/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/ftp", "UiPath.FTP.Activities.Design")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("7d1dc1a6-aa81-4e6a-9081-97bf21662f4e")]
---
+++ Activities/FTP/UiPath.FTP.Activities/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/ftp", "UiPath.FTP.Activities")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/ftp", "UiPath.FTP.Activities.Design")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("fdfc92ee-090c-4e23-9422-f90eb94c43dd")]
