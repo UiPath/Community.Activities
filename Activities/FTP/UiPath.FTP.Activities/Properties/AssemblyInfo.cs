ffac5d0ee14aa3e2f4aa0ec5eecbfc5960b12bcd
Alexandru Stefan <alexandru.stefan@24software.ro>
2021-02-17 13:18:08 +0200

    FTP project migrated to net50-windows

--- Activities/FTP/UiPath.FTP.Activities/Properties/AssemblyInfo.cs
+++ Activities/FTP/UiPath.FTP.Activities/Properties/AssemblyInfo.cs
@@ -1,17 +1,16 @@
-﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("UiPath.FTP.Activities")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/ftp", "UiPath.FTP.Activities")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities/ftp", "UiPath.FTP.Activities.Design")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
 [assembly: Guid("fdfc92ee-090c-4e23-9422-f90eb94c43dd")]
