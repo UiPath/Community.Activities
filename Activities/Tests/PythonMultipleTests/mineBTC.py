bf097a1fd6a9ed23eb2a2d839cec5af6f40ddad7
Vlad Nicula <vlad.nicula@24software.ro>
2021-02-17 17:36:55 +0200

    Corrected Tests for version 2.7

--- Activities/Tests/PythonMultipleTests/mineBTC.py
+++ Activities/Tests/PythonMultipleTests/mineBTC.py
@@ -10,21 +10,18 @@ def mine(block_number, transactions, previous_hash, prefix_zeros):
         text = str(block_number) + transactions + previous_hash + str(nonce)
         new_hash = SHA256(text)
         if new_hash.startswith(prefix_str):
-            print(f"Yay! Successfully mined bitcoins with nonce value: {nonce}")
             return new_hash
 
-    raise BaseException(f"Couldn't find correct has after trying {MAX_NONCE} times")
+    raise BaseException("Couldn't find correct hash")
 
 if __name__=='__main__':
     transactions=''''
     Dhaval->Bhavin->20,
     Mando->Cara->45
     '''
-    difficulty=8 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
+    difficulty=2 # try changing this to higher number and you will see it will take more time for mining as difficulty increases
     import time
     start = time.time()
-    print("start mining")
     new_hash = mine(5,transactions,'0000000xa036944e29568d0cff17edbe038f81208fecf9a66be9a2b8321c6ec7', difficulty)
     total_time = str((time.time() - start))
-    print(f"end mining. Mining took: {total_time} seconds")
     print(new_hash)
