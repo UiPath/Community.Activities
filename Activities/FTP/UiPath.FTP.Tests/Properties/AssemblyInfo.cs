058f9f74586bcb32da00ae5c0ce5928efbe90022
Alexandru Stefan <alexandru.stefan@24software.ro>
2021-02-23 14:15:56 +0200

    fix test project

--- Activities/FTP/UiPath.FTP.Tests/Properties/AssemblyInfo.cs
+++ Activities/FTP/UiPath.FTP.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UiPath.FTP.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UiPath.FTP.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("d35adf63-dd01-476a-8d15-cd3fb412ae5d")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
