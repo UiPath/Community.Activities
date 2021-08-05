3d5e99f387e897cfd166d02ad7b3fa861d5152e9
Radu Negulescu <radu.negulescu@24software.ro>
2021-08-05 12:48:48 +0300

    Localization Fixes for OSAC-206

--- Activities/Credentials/UiPath.Credentials.Activities/DesignerMetadata.cs
+++ Activities/Credentials/UiPath.Credentials.Activities/DesignerMetadata.cs
@@ -1,4 +1,7 @@
-﻿using System.Activities.Presentation.Metadata;
+﻿using CredentialManagement;
+using System;
+using System.Activities;
+using System.Activities.Presentation.Metadata;
 using System.ComponentModel;
 using UiPath.Credentials.Activities.Properties;
 
@@ -22,6 +25,30 @@ namespace UiPath.Credentials.Activities
             builder.AddCustomAttributes(typeof(DeleteCredential), Resources.Result, new CategoryAttribute(Resources.Output));
             builder.AddCustomAttributes(typeof(GetSecureCredential), Resources.Result, new CategoryAttribute(Resources.Output));
             builder.AddCustomAttributes(typeof(RequestCredential), Resources.Result, new CategoryAttribute(Resources.Output));
+
+            // DisplayNames
+            builder.AddCustomAttributes(typeof(GetSecureCredential), new DisplayNameAttribute(Resources.Activity_GetSecureCredential_Property_GetSecureCredentialDisplayName_Name));
+            builder.AddCustomAttributes(typeof(AddCredential), new DisplayNameAttribute(Resources.Activity_AddCredential_Property_AddCredentialDisplayName_Name));
+            builder.AddCustomAttributes(typeof(DeleteCredential), new DisplayNameAttribute(Resources.Activity_DeleteCredential_Property_DeleteCredentialDisplayName_Name));
+            builder.AddCustomAttributes(typeof(RequestCredential), new DisplayNameAttribute(Resources.Activity_RequestCredential_Property_RequestCredentialDisplayName_Name));
+
+            // Descriptions
+            builder.AddCustomAttributes(typeof(GetSecureCredential), new DescriptionAttribute(Resources.Activity_GetSecureCredential_Property_GetSecureCredentialDescription_Description));
+            builder.AddCustomAttributes(typeof(AddCredential), new DescriptionAttribute(Resources.Activity_AddCredential_Property_AddCredentialDescription_Description));
+            builder.AddCustomAttributes(typeof(DeleteCredential), new DescriptionAttribute(Resources.Activity_DeleteCredential_Property_DeleteCredentialDescription_Description));
+            builder.AddCustomAttributes(typeof(RequestCredential), new DescriptionAttribute(Resources.Activity_RequestCredential_Property_RequestCredentialDescription_Description));
+
+            // GetSecureCredential Properties
+            builder.AddCustomAttributes(typeof(GetSecureCredential), nameof(GetSecureCredential.Result), new DescriptionAttribute(Resources.Result));
+            
+            // AddCredential Properties
+            builder.AddCustomAttributes(typeof(AddCredential), nameof(AddCredential.Result), new DescriptionAttribute(Resources.Result));
+            
+            // DeleteCredential Properties
+            builder.AddCustomAttributes(typeof(DeleteCredential), nameof(DeleteCredential.Result), new DescriptionAttribute(Resources.Result));
+            
+            // RequestCredential Properties
+            builder.AddCustomAttributes(typeof(RequestCredential), nameof(RequestCredential.Result), new DescriptionAttribute(Resources.Result));
 
             MetadataStore.AddAttributeTable(builder.CreateTable());
         }
