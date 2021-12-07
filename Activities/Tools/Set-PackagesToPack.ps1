085b95a6842cfed6586d70ec5eac6716231cf37b
catalin-marin <catalin.marin@uipath.com>
2021-12-07 13:30:12 +0200

    add debug info

--- Activities/Tools/Set-PackagesToPack.ps1
+++ Activities/Tools/Set-PackagesToPack.ps1
@@ -10,49 +10,55 @@ param(
 
 function Main
 {
-    Write-Verbose $affectedActivities
+    Write-Host "dir: $dir"
+    Write-Host "affectedActivities: $affectedActivities"
+
     $solutions = @()
     if($affectedActivities)
     {
         $solutions = $affectedActivities -split "," | Where-Object { !([string]::IsNullOrWhiteSpace($_.ToString())) }
         $solutions = Get-ChildItem -Path $dir *.sln | Where-Object { Is-ActivitySolution $solutions $_  }
-        $solutions | ForEach-Object { Write-Verbose $_.Name }
+        $solutions | ForEach-Object { Write-Host "solution: $($_.Name)" }
     }
-
-    $files = Get-ChildItem -Path $dir *.csproj -Recurse -ErrorAction SilentlyContinue -ErrorVariable err
-    Validate-Long-Paths $err
 
     if($solutions.Length -eq 0)
     {
+        Write-Host "No solutions found"
         return;
     }
+    Write-Host "solutions:" $solutions
+
+
+    $files = Get-ChildItem -Path $dir *.csproj -Recurse -ErrorAction SilentlyContinue -ErrorVariable err
+    Validate-Long-Paths $err
+    Write-Host "files: $files"
 
     $packFiles = $files | Where-Object { Is-Package $solutions $_};
     $pattern = ''
     foreach($file in $packFiles) 
     {
-        Write-Verbose $file
+        Write-Host "pack file:" $file
         $pattern += "**\$file;" 
     }
     if($pattern)
     {
-        Write-Verbose $pattern;
-        Write-Host \"##vso[task.setvariable variable=PackagesToPack;]$pattern";
-        Write-Host \"##vso[task.setvariable variable=HasNuspecPackages;]true";
+        Write-Host "pack pattern:" $pattern;
+        Write-Host "##vso[task.setvariable variable=PackagesToPack;]$pattern";
+        Write-Host "##vso[task.setvariable variable=HasNuspecPackages;]true";
     }
 
     $skdFiles = $files | Where-Object { Is-SdkPackage $solutions $_};
     $pattern = ''
     foreach($file in $skdFiles) 
     {
-        Write-Verbose $file
+        Write-Host "sdk file:" $file
         $pattern += "**\$file;" 
     }
     if($pattern)
     {
-        Write-Verbose $pattern;
-        Write-Host \"##vso[task.setvariable variable=SdkPackagesToPack;]$pattern";
-        Write-Host \"##vso[task.setvariable variable=HasSdkPackages;]true";
+        Write-Host "sdk pattern:" $pattern;
+        Write-Host "##vso[task.setvariable variable=SdkPackagesToPack;]$pattern";
+        Write-Host "##vso[task.setvariable variable=HasSdkPackages;]true";
     }
 }
 
@@ -83,12 +89,9 @@ function Is-Package($solutions, $projFile)
     {
         return $false;
     }
+    Write-Host "found projFile:" $projFile.FullName;
     $nuspec = $projFile.FullName -replace '.csproj','.nuspec';
-    if(Test-Path $nuspec)
-    {
-        return $true;
-    }
-    return $false;
+    return (Test-Path $nuspec)
 }
 
 function Is-SdkPackage($solutions, $projFile)
@@ -106,12 +109,9 @@ function Is-SdkPackage($solutions, $projFile)
     {
         return $false;
     }
+    Write-Host "found sdk projFile:" $projFile.FullName;
     $content = Get-Content $projFile.FullName;
-    if(($content | Select-String "<PackageId>") -and ($content | Select-String "<VersionSuffix>"))
-    {
-        return $true;
-    }
-    return $false;
+    return (($content | Select-String "<PackageId>") -and ($content | Select-String "<VersionSuffix>"))
 }
 
 function Validate-Long-Paths($err)
