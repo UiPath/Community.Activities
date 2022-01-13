e69684da810b7e1138909a8e78f7592b0d0ad91e
Radu Negulescu <radu.negulescu@24software.ro>
2022-01-13 13:25:04 +0200

    OSAC-270, OSAC-328 - Execute Query throws error at runtime that Value Cannot be null even activity is valid in Studio Web

--- Activities/Database/UiPath.Database.Activities/Properties/UiPath.Database.Activities.Designer.cs
+++ Activities/Database/UiPath.Database.Activities/Properties/UiPath.Database.Activities.Designer.cs
@@ -19,7 +19,7 @@ namespace UiPath.Database.Activities.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UiPath_Database_Activities {
@@ -1486,6 +1486,15 @@ namespace UiPath.Database.Activities.Properties {
         public static string ValidationError_ConnectionMustNotBeNull {
             get {
                 return ResourceManager.GetString("ValidationError_ConnectionMustNotBeNull", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Existing connection is invalid. Please ensure the connection can be established..
+        /// </summary>
+        public static string ValidationError_ConnectionNotValid {
+            get {
+                return ResourceManager.GetString("ValidationError_ConnectionNotValid", resourceCulture);
             }
         }
         
