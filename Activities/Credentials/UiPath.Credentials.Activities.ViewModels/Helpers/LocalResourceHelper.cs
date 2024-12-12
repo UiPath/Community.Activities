514fd2b970c52713fbe5b2570c259c47c13faeee
viogroza <86362427+viogroza@users.noreply.github.com>
2024-12-12 15:53:17 +0200

    Credentials: Type cache (24.12) [STUD-72720] (#454)
    * Credentials: Type cache (24.12) [STUD-72720]
    
    Switch from Net5 to Net6
    Add view models project
    Add view models and resources
    Update pipeline to build for Net6
    Fix package id
    
    * Credentials: Use agent that builds with .net 6
    
    ---------
    
    Co-authored-by: Alexandru Petre <93531170+alexandru-petre@users.noreply.github.com>

---
+++ Activities/Credentials/UiPath.Credentials.Activities.ViewModels/Helpers/LocalResourceHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Activities.DesignViewModels;
+using System.Activities;
+
+namespace UiPath.Activities.Credentials.ViewModels.Helpers
+{
+    //We need to reference System.Activities in order for the view models to work
+    internal static class LocalResourceHelper
+    {
+        public static object GetNormalizedLocalPath(string updatedPropertyName, string targetPropertyName, object value)
+        {
+            if (targetPropertyName.Equals(updatedPropertyName))
+            {
+                var argument = value as InArgument<string>;
+                return argument?.GetNormalizedPath();
+            }
+            return value;
+        }
+
+        public static InArgument<string> GetNormalizedPath(this InArgument<string> argument)
+        {
+            if (argument == null || argument.Expression == null || !argument.Expression.IsLiteral())
+            {
+                return argument;
+            }
+
+            string path = argument.Expression.ToString();
+            return path.GetNormalizedPathInternal();
+        }
+
+        public static InArgument<string> GetNormalizedPath(this DesignInArgument<string> argument)
+        {
+            if (!argument.TryGetLiteralValue(out var value))
+            {
+                return argument.Value;
+            }
+
+            string path = (string)value;
+            return path.GetNormalizedPathInternal();
+        }
+
+        private static InArgument<string> GetNormalizedPathInternal(this string path)
+        {
+            if (!string.IsNullOrWhiteSpace(path))
+            {
+                path = path.Replace('\\', '/').Replace("//", "/");
+            }
+            return path;
+        }
+    }
+}
