353ca0fe968ae3e83bae4d3dcb82cac12fe63769
viogroza <viorel.groza@uipath.com>
2025-05-28 19:56:56 +0300

    Python: Cross-platform support (25.6) [STUD-75430]
    Add view models for all activities
    Remove Python.Runtime.Unix.dll (now use the same dll on all platforms)
    Make PythonHost cross-platform
    Use dotnet to start the PythonHost on all platforms
    Set default target to x64 for PythonScope
    Fix build problem for all activities (changing files required rebuild)
    Remove UiPath.Activities.Design project
    Remove some unused files

---
+++ Activities/Python/UiPath.Python.Activities.ViewModels/ViewModels/PythonScopeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Activities.DesignViewModels;
+using System.Diagnostics.CodeAnalysis;
+using UiPath.Studio.Activities.Api;
+using UiPath.Python;
+
+namespace UiPath.Activities.Python.ViewModels
+{
+    [ExcludeFromCodeCoverage]
+    class PythonScopeViewModel : DesignPropertiesViewModel
+    {
+        public PythonScopeViewModel(IDesignServices services) : base(services)
+        {
+            //Force dependency here for activities.api
+            //Otherwise this assembly will be ignored by studio
+            //see STUD-73622
+            var _ = services.GetService<IWorkflowDesignApi>();
+        }
+
+        public DesignProperty<Version> Version { get; set; }
+
+        public DesignInArgument<string> Path { get; set; }
+
+        public DesignInArgument<string> LibraryPath { get; set; }
+
+        public DesignProperty<TargetPlatform> TargetPlatform { get; set; }
+
+        public DesignInArgument<string> WorkingFolder { get; set; }
+
+        public DesignInArgument<double> OperationTimeout { get; set; }
+    }
+}
