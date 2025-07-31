18a33336a40dd0fe014e70a3d15090d64d982f36
viogroza <viorel.groza@uipath.com>
2025-07-31 18:46:09 +0300

    FTP: EnumerateObjects filter (25.8) [STUD-76148]
    Make sure that every value in the enum has only 1 bit set
    Update unitetst

--- Activities/FTP/UiPath.FTP.Activities/EnumerateObjects.cs
+++ Activities/FTP/UiPath.FTP.Activities/EnumerateObjects.cs
@@ -27,8 +27,8 @@ namespace UiPath.FTP.Activities
         [LocalizedCategory(nameof(Resources.Options))]
         [LocalizedDisplayName(nameof(Resources.Activity_EnumerateObjects_Property_Filter_Name))]
         [LocalizedDescription(nameof(Resources.Activity_EnumerateObjects_Property_Filter_Description))]
-        [DefaultValue(FtpFilterObjectType.Directory | FtpFilterObjectType.File)]
-        public FtpFilterObjectType Filter { get; set; } = FtpFilterObjectType.All;
+        [DefaultValue(FtpFilterObjectType.Directory | FtpFilterObjectType.File | FtpFilterObjectType.Link | FtpFilterObjectType.Other)]
+        public FtpFilterObjectType Filter { get; set; } = FtpFilterObjectType.Directory | FtpFilterObjectType.File | FtpFilterObjectType.Link | FtpFilterObjectType.Other;
 
         [LocalizedCategory(nameof(Resources.Output))]
         [LocalizedDisplayName(nameof(Resources.Activity_EnumerateObjects_Property_Files_Name))]
@@ -54,16 +54,20 @@ namespace UiPath.FTP.Activities
                 files = Enumerable.Empty<FtpObjectInfo>();
             }
             //filter based on what the user selection
-            else if (Filter != FtpFilterObjectType.All)
+            else if (Filter != (FtpFilterObjectType.Directory | FtpFilterObjectType.File | FtpFilterObjectType.Link | FtpFilterObjectType.Other))
             {
+                bool includeDirectories = (Filter & FtpFilterObjectType.Directory) != 0;
+                bool includeFiles = (Filter & FtpFilterObjectType.File) != 0;
+                bool includeLinks = (Filter & FtpFilterObjectType.Link) != 0;
+                bool includeOthers = (Filter & FtpFilterObjectType.Other) != 0;
                 files = files.Where(x =>
                 {
                     return x.Type switch
                     {
-                        FtpObjectType.Directory => (Filter & FtpFilterObjectType.Directory) != 0,
-                        FtpObjectType.File => (Filter & FtpFilterObjectType.File) != 0,
-                        FtpObjectType.Link => (Filter & FtpFilterObjectType.Link) != 0,
-                        FtpObjectType.Other => (Filter & FtpFilterObjectType.Other) != 0,
+                        FtpObjectType.Directory => includeDirectories,
+                        FtpObjectType.File => includeFiles,
+                        FtpObjectType.Link => includeLinks,
+                        FtpObjectType.Other => includeOthers,
                         _ => false,
                     };
                 });
