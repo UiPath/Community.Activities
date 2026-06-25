2562189f8149e21e006df5efa647b19ce1f73766
viogroza <viorel.groza@uipath.com>
2026-06-25 20:29:14 +0300

    Cryptography: move cryptography property metadata to ViewModels and align category visibility defaults [STUD-80610]
    DecryptCryptoViewModelBase
    EncryptCryptoViewModelBase
    DecryptFileViewModel
    DecryptTextViewModel
    EncryptFileViewModel
    EncryptTextViewModel
    KeyedHashFileViewModel
    KeyedHashTextViewModel
    PgpGenerateKeysViewModel
    PgpSignViewModelBase
    PgpSignFileViewModel
    PgpClearSignFileViewModel
    PgpVerifyViewModel
    ViewModelPropertyMetadataHelper
    generated by GitHub Copilot

--- Activities/Cryptography/UiPath.Cryptography.Activities/NetCore/ViewModels/DecryptTextViewModel.cs
+++ Activities/Cryptography/UiPath.Cryptography.Activities/NetCore/ViewModels/DecryptTextViewModel.cs
@@ -32,6 +32,7 @@ namespace UiPath.Cryptography.Activities.NetCore.ViewModels
             var orderIndex = 1;
 
             Input.IsPrincipal = true;
+            Input.IsRequired = true;
             Input.OrderIndex = orderIndex++;
             Input.Category = Resources.Input;
 
@@ -44,6 +45,7 @@ namespace UiPath.Cryptography.Activities.NetCore.ViewModels
             Result.Category = Resources.Output;
 
             ConfigureTailProperties(ref orderIndex);
+            ConfigurePropertyTexts();
             ConfigureKeyInputModeMenuActions();
             ConfigurePublicKeyFileMenuActions();
             ConfigurePassphraseInputModeMenuActions();
@@ -52,6 +54,46 @@ namespace UiPath.Cryptography.Activities.NetCore.ViewModels
         protected override void OnAlgorithmChanged(bool isPgp)
         {
             PlaintextEncodingString.IsVisible = !isPgp;
+        }
+
+        private void ConfigurePropertyTexts()
+        {
+            Algorithm.DisplayName = Resources.Activity_DecryptText_Property_Algorithm_Name;
+            Algorithm.Tooltip = Resources.Activity_DecryptText_Property_Algorithm_Description;
+            Input.DisplayName = Resources.Activity_DecryptText_Property_Input_Name;
+            Input.Tooltip = Resources.Activity_DecryptText_Property_Input_Description;
+            Key.DisplayName = Resources.Activity_DecryptText_Property_Key_Name;
+            Key.Tooltip = Resources.Activity_DecryptText_Property_Key_Description;
+            KeySecureString.DisplayName = Resources.Activity_DecryptText_Property_KeySecureString_Name;
+            KeySecureString.Tooltip = Resources.Activity_DecryptText_Property_KeySecureString_Description;
+            KeyEncodingString.DisplayName = Resources.Activity_DecryptText_Property_KeyEncodingString_Name;
+            KeyEncodingString.Tooltip = Resources.Activity_DecryptText_Property_KeyEncodingString_Description;
+            PlaintextEncodingString.DisplayName = Resources.Activity_DecryptText_Property_PlaintextEncodingString_Name;
+            PlaintextEncodingString.Tooltip = Resources.Activity_DecryptText_Property_PlaintextEncodingString_Description;
+            Format.DisplayName = Resources.Activity_DecryptText_Property_Format_Name;
+            Format.Tooltip = Resources.Activity_DecryptText_Property_Format_Description;
+            KeyFormat.DisplayName = Resources.Activity_DecryptText_Property_KeyFormat_Name;
+            KeyFormat.Tooltip = Resources.Activity_DecryptText_Property_KeyFormat_Description;
+            KdfIterations.DisplayName = Resources.Activity_DecryptText_Property_KdfIterations_Name;
+            KdfIterations.Tooltip = Resources.Activity_DecryptText_Property_KdfIterations_Description;
+            AesKeySize.DisplayName = Resources.Activity_DecryptText_Property_AesKeySize_Name;
+            AesKeySize.Tooltip = Resources.Activity_DecryptText_Property_AesKeySize_Description;
+            ContinueOnError.DisplayName = Resources.Activity_DecryptText_Property_ContinueOnError_Name;
+            ContinueOnError.Tooltip = Resources.Activity_DecryptText_Property_ContinueOnError_Description;
+            PrivateKeyFilePath.DisplayName = Resources.Activity_DecryptText_Property_PrivateKeyFilePath_Name;
+            PrivateKeyFilePath.Tooltip = Resources.Activity_DecryptText_Property_PrivateKeyFilePath_Description;
+            Passphrase.DisplayName = Resources.Activity_DecryptText_Property_Passphrase_Name;
+            Passphrase.Tooltip = Resources.Activity_DecryptText_Property_Passphrase_Description;
+            PassphraseSecureString.DisplayName = Resources.Activity_DecryptText_Property_PassphraseSecureString_Name;
+            PassphraseSecureString.Tooltip = Resources.Activity_DecryptText_Property_PassphraseSecureString_Description;
+            VerifySignature.DisplayName = Resources.Activity_DecryptText_Property_VerifySignature_Name;
+            VerifySignature.Tooltip = Resources.Activity_DecryptText_Property_VerifySignature_Description;
+            PublicKeyFilePath.DisplayName = Resources.Activity_DecryptText_Property_PublicKeyFilePath_Name;
+            PublicKeyFilePath.Tooltip = Resources.Activity_DecryptText_Property_PublicKeyFilePath_Description;
+            PublicKeyFile.DisplayName = Resources.Activity_DecryptText_Property_PublicKeyFile_Name;
+            PublicKeyFile.Tooltip = Resources.Activity_DecryptText_Property_PublicKeyFile_Description;
+            Result.DisplayName = Resources.Activity_DecryptText_Property_Result_Name;
+            Result.Tooltip = Resources.Activity_DecryptText_Property_Result_Description;
         }
     }
 }
--- Activities/Cryptography/UiPath.Cryptography.Activities/NetCore/ViewModels/PgpClearSignFileViewModel.cs
+++ Activities/Cryptography/UiPath.Cryptography.Activities/NetCore/ViewModels/PgpClearSignFileViewModel.cs
@@ -30,5 +30,29 @@ namespace UiPath.Cryptography.Activities.NetCore.ViewModels
             ClearSignedFile.OrderIndex = orderIndex;
             ClearSignedFile.Category = Resources.Output;
         }
+
+        protected override void ConfigurePropertyTexts()
+        {
+            InputFilePath.DisplayName = Resources.Activity_PgpClearSignFile_Property_InputFilePath_Name;
+            InputFilePath.Tooltip = Resources.Activity_PgpClearSignFile_Property_InputFilePath_Description;
+            InputFile.DisplayName = Resources.Activity_PgpClearSignFile_Property_InputFile_Name;
+            InputFile.Tooltip = Resources.Activity_PgpClearSignFile_Property_InputFile_Description;
+            PrivateKeyFilePath.DisplayName = Resources.Activity_PgpClearSignFile_Property_PrivateKeyFilePath_Name;
+            PrivateKeyFilePath.Tooltip = Resources.Activity_PgpClearSignFile_Property_PrivateKeyFilePath_Description;
+            PrivateKeyFile.DisplayName = Resources.Activity_PgpClearSignFile_Property_PrivateKeyFile_Name;
+            PrivateKeyFile.Tooltip = Resources.Activity_PgpClearSignFile_Property_PrivateKeyFile_Description;
+            Passphrase.DisplayName = Resources.Activity_PgpClearSignFile_Property_Passphrase_Name;
+            Passphrase.Tooltip = Resources.Activity_PgpClearSignFile_Property_Passphrase_Description;
+            PassphraseSecureString.DisplayName = Resources.Activity_PgpClearSignFile_Property_PassphraseSecureString_Name;
+            PassphraseSecureString.Tooltip = Resources.Activity_PgpClearSignFile_Property_PassphraseSecureString_Description;
+            OutputFilePath.DisplayName = Resources.Activity_PgpClearSignFile_Property_OutputFilePath_Name;
+            OutputFilePath.Tooltip = Resources.Activity_PgpClearSignFile_Property_OutputFilePath_Description;
+            Overwrite.DisplayName = Resources.Activity_PgpClearSignFile_Property_Overwrite_Name;
+            Overwrite.Tooltip = Resources.Activity_PgpClearSignFile_Property_Overwrite_Description;
+            ContinueOnError.DisplayName = Resources.Activity_PgpClearSignFile_Property_ContinueOnError_Name;
+            ContinueOnError.Tooltip = Resources.Activity_PgpClearSignFile_Property_ContinueOnError_Description;
+            ClearSignedFile.DisplayName = Resources.Activity_PgpClearSignFile_Property_ClearSignedFile_Name;
+            ClearSignedFile.Tooltip = Resources.Activity_PgpClearSignFile_Property_ClearSignedFile_Description;
+        }
     }
 }
